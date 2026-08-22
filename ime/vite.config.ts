5904e4c434d5827169ffdf18d1ef2737e388bc1e
jgoveafamily-arch <jgoveafamily@gmail.com>
2026-08-21 20:16:37 -0500

    Update vite.config.ts

--- ime/vite.config.ts
+++ ime/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    host: true, // or '0.0.0.0'
+    host: true, // Listens on all local IPs
+    port: 3000, // Or whatever port your project defaults to
     hmr: {
-      clientPort: 443,
-    }
-  }
-})
-
+      clientPort: 443, // Routes WebSocket connections through Codespaces' HTTPS proxy
+    },
+  },
+});
